23d37dbb4caadb18f54aa593fbee30b5ef59cdb8
Bern <116868656+Bern-lab@users.noreply.github.com>
2026-06-10 08:16:40 +0200

    Route toe stair reward to temporal implementation

--- src/mjlab/tasks/velocity/mdp/__init__.py
+++ src/mjlab/tasks/velocity/mdp/__init__.py
@@ -3,6 +3,10 @@ from mjlab.envs.mdp import *  # noqa: F401, F403
 from .curriculums import *  # noqa: F403
 from .observations import *  # noqa: F403
 from .rewards import *  # noqa: F403
+from . import temporal_stair_rewards as _temporal_stair_rewards
 from .target_heading_command import *  # noqa: F403
 from .terminations import *  # noqa: F403
 from .velocity_command import *  # noqa: F403
+
+toe_step_riser_slab_penalty = _temporal_stair_rewards.toe_step_riser_slab_penalty
+toe_step_riser_approach_penalty = _temporal_stair_rewards.toe_step_riser_approach_penalty
